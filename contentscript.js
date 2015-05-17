b95535cf17fb63a95de06f5814b14ed407c47e01
Oded <odedg@wix.com>
2015-05-18 00:25:33 +0300

    add append link method

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,13 @@
+var appendLinkToDescription = function (description, link) {
+    var a = document.createElement('a');
+    a.href = link;
+    a.target = "_blank";
+    a.innerHTML = "link";
+    description.appendChild(a);
+};
+
 var buildScriptProblemsWrapper = document.getElementsByClassName("expandCollapseContainer")[0].children[0];
 var problemDescriptions = buildScriptProblemsWrapper.getElementsByClassName("problemDescription");
-
 var arrayLength = problemDescriptions.length;
 for (var i = 0; i < arrayLength; i++) {
     var description = problemDescriptions[i];
@@ -8,11 +15,9 @@ for (var i = 0; i < arrayLength; i++) {
     var link = descriptionText.match(/Screenshot link: (.*)/)[1];
     var decodedLink = link.replace(/&amp;/g, '&');
     description.innerHTML = descriptionText.replace(/Screenshot link: (.*)/, "Screenshot: ");
-    var a = document.createElement('a');
-    a.target="_blank";
-    a.href = decodedLink;
-    a.innerHTML = "link";
-    description.appendChild(a);
+    appendLinkToDescription(description, decodedLink);
 }
+
+
 
 
