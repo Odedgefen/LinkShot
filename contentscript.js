a11c41aa924eb347f977c74f938520bfd764081b
Oded <odedg@wix.com>
2015-05-18 00:15:23 +0300

    make code work for all links

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,17 @@
 var descriptions = document.getElementsByClassName("problemDescription");
-var firstDesc = descriptions[0];
-var descriptionText = firstDesc.innerHTML;
-var link = descriptionText.match(/Screenshot link: (.*)/)[1]
-var decodedLink = link.replace(/&amp;/g, '&');
-var newSpanDescriptionText = descriptionText.replace(/Screenshot link: (.*)/, "Screenshot: ");
-firstDesc.innerHTML = newSpanDescriptionText;
 
-var a = document.createElement('a');
-a.href = decodedLink;
-a.innerHTML = "link";
-firstDesc.appendChild(a);
+var arrayLength = descriptions.length;
+for (var i = 0; i < arrayLength - 1; i++) {
+    var description = descriptions[i];
+    var descriptionText = description.innerHTML;
+    var link = descriptionText.match(/Screenshot link: (.*)/)[1];
+    var decodedLink = link.replace(/&amp;/g, '&');
+    description.innerHTML = descriptionText.replace(/Screenshot link: (.*)/, "Screenshot: ");
+    var a = document.createElement('a');
+    a.target="_blank";
+    a.href = decodedLink;
+    a.innerHTML = "link";
+    description.appendChild(a);
+}
+
+
