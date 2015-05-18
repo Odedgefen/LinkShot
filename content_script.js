bd4e5662846afb55c190fb38a8dae932e16aeff2
Oded <odedg@wix.com>
2015-05-18 12:48:01 +0300

    put inside method

--- content_script.js
+++ content_script.js
@@ -13,15 +13,19 @@ var extractDecodedLinkFromString = function (str) {
     return link.replace(/&amp;/g, '&');
 };
 
-var buildScriptProblemsWrapper = document.getElementsByClassName("expandCollapseContainer")[0].children[0];
-var problemDescriptions = buildScriptProblemsWrapper.getElementsByClassName("problemDescription");
-var numOfProblems = problemDescriptions.length;
-for (var i = 0; i < numOfProblems; i++) {
-    var descriptionText = problemDescriptions[i].innerHTML;
-    var decodedLink = extractDecodedLinkFromString(descriptionText);
-    problemDescriptions[i].innerHTML = descriptionText.match(linkRegex)[1];
-    appendLinkToDescription(problemDescriptions[i], decodedLink);
-}
+var linkBuildProblems = function () {
+    var buildScriptProblemsWrapper = document.getElementsByClassName("expandCollapseContainer")[0].children[0];
+    var problemDescriptions = buildScriptProblemsWrapper.getElementsByClassName("problemDescription");
+    var numOfProblems = problemDescriptions.length;
+    for (var i = 0; i < numOfProblems; i++) {
+        var descriptionText = problemDescriptions[i].innerHTML;
+        var decodedLink = extractDecodedLinkFromString(descriptionText);
+        problemDescriptions[i].innerHTML = descriptionText.match(linkRegex)[1];
+        appendLinkToDescription(problemDescriptions[i], decodedLink);
+    }
+};
+
+linkBuildProblems();
 
 
 
